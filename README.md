3d346b36c2e07ded2a374514440abdddb8c1c89f
Adam Gutglick <adamgsal@gmail.com>
2020-04-11 21:14:59 +0300

    Some initial work

---
+++ README.md
@@ -0,0 +1,4 @@
+# rilo
+writing [kilo](http://antirez.com/news/108) in rust, loosely following [this](https://viewsourcecode.org/snaptoken/kilo/index.html) tutorial.
+
+WARNING: rilo may make your terminal behave kina wierd
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::io::{self, BufRead, Error, Read, Write};
+use std::os::unix::prelude::*;
+use termios::*;
+
+fn ctrl_key(c: char) -> char {
+    (c as u8 & 0x1f).into()
+}
+
+fn main() -> io::Result<()> {
+    // Initial terminal setup
+    let initial_state = enable_raw_mode();
+    refresh_screen();
+
+    let mut buffer = [0_u8; 1];
+    loop {
+        for b in io::stdin()
+            .lock()
+            .bytes()
+            .map(|x| x.and_then(|y| Ok(y as char)))
+        {
+            match b {
+                Ok(c) => {
+                    if c == ctrl_key('q') {
+                        refresh_screen();
+                        std::process::exit(0);
+                    }
+
+                    if c == ctrl_key('x') {
+                        refresh_screen();
+                        continue;
+                    }
+
+                    if !c.is_control() {
+                        print!("I Got: {}\r\n", c);
+                    } else {
+                        print!("Code: {}\r\n", c as u8);
+                    }
+                }
+                Err(_) => {
+                    eprintln!("Error reading key press");
+                    std::process::exit(1);
+                }
+            }
+        }
+    }
+
+    termios::tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &initial_state);
+    Ok(())
+}
+
+fn enable_raw_mode() -> Termios {
+    let fd = io::stdin().as_raw_fd();
+    let mut term = Termios::from_fd(fd).unwrap();
+    let mut initial = term.clone();
+
+    term.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    term.c_oflag &= !(OPOST);
+    term.c_cflag |= (CS8);
+    term.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+    term.c_cc[VMIN] = 0;
+    term.c_cc[VTIME] = 1;
+
+    termios::tcsetattr(fd, TCSAFLUSH, &term);
+    initial
+}
+
+fn refresh_screen() {
+    print!("\x1B[2J");
+    print!("\x1b[H");
+    io::stdout().flush();
+}
