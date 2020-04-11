6179606d4079301e72ca026b9480d1d7fed2c0c1
Adam Gutglick <adamgsal@gmail.com>
2020-04-11 23:05:47 +0300

    Created Editor sturct to handle some state

--- src/main.rs
+++ src/main.rs
@@ -6,66 +6,101 @@ fn ctrl_key(c: char) -> char {
     (c as u8 & 0x1f).into()
 }
 
+struct RawMode {
+    inner: Termios,
+}
+
+impl RawMode {
+    fn enable_raw_mode() -> Self {
+        let fd = io::stdin().as_raw_fd();
+        let mut term = Termios::from_fd(fd).unwrap();
+        let mut raw_mode = Self {
+            inner: term.clone(),
+        };
+
+        term.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        term.c_oflag &= !(OPOST);
+        term.c_cflag |= (CS8);
+        term.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+        term.c_cc[VMIN] = 0;
+        term.c_cc[VTIME] = 1;
+
+        termios::tcsetattr(fd, TCSAFLUSH, &term);
+        raw_mode
+    }
+}
+
+impl Drop for RawMode {
+    fn drop(&mut self) {
+        termios::tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &self.inner);
+    }
+}
+
+struct Editor {
+    mode: RawMode,
+}
+
+impl Editor {
+    pub fn new() -> Self {
+        Editor {
+            mode: RawMode::enable_raw_mode(),
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     // Initial terminal setup
-    let initial_state = enable_raw_mode();
+    let mut e = Editor::new();
     refresh_screen();
+    draw_rows();
+    goto_start();
 
-    loop {
-        for b in io::stdin()
-            .lock()
-            .bytes()
-            .map(|x| x.and_then(|y| Ok(y as char)))
-        {
-            match b {
-                Ok(c) => {
-                    if c == ctrl_key('q') {
-                        refresh_screen();
-                        termios::tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &initial_state);
-                        std::process::exit(0);
-                    }
+    let mut one_buff = [0; 1];
 
-                    if c == ctrl_key('x') {
-                        refresh_screen();
-                        continue;
-                    }
+    while let len = io::stdin().read(&mut one_buff) {
+        if len.unwrap() != 0 {
+            let c = one_buff[0] as char;
+            if c == ctrl_key('q') {
+                refresh_screen();
+                std::mem::drop(e);
+                break;
+            }
 
-                    if !c.is_control() {
-                        print!("I Got: {}\r\n", c);
-                    } else {
-                        print!("Code: {}\r\n", c as u8);
-                    }
-                }
-                Err(_) => {
-                    eprintln!("Error reading key press");
-                    std::process::exit(1);
-                }
+            if c == ctrl_key('x') {
+                refresh_screen();
+                continue;
+            }
+
+            if !c.is_control() {
+                print!("I Got: {}\r\n", c);
+            } else {
+                print!("Code: {}\r\n", c as u8);
             }
         }
     }
 
-    termios::tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &initial_state);
     Ok(())
 }
 
-fn enable_raw_mode() -> Termios {
-    let fd = io::stdin().as_raw_fd();
-    let mut term = Termios::from_fd(fd).unwrap();
-    let mut initial = term.clone();
-
-    term.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    term.c_oflag &= !(OPOST);
-    term.c_cflag |= (CS8);
-    term.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
-    term.c_cc[VMIN] = 0;
-    term.c_cc[VTIME] = 1;
-
-    termios::tcsetattr(fd, TCSAFLUSH, &term);
-    initial
+fn refresh_screen() {
+    clear_screen();
+    goto_start();
 }
 
-fn refresh_screen() {
+fn clear_screen() {
     print!("\x1B[2J");
+    io::stdout().flush();
+}
+
+fn goto_start() {
     print!("\x1b[H");
+    io::stdout().flush();
+}
+
+fn draw_rows() {
+    for _ in 0..24 {
+        print!("~\r\n");
+    }
+
     io::stdout().flush();
 }
