1401ddb09cb63cc3d95579406a75a6386987ebaf
Adam Gutglick <adamgsal@gmail.com>
2020-04-16 15:53:35 +0300

    Stupid script to make a release executable

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cargo clean 2> /dev/null && cargo build --release 2> /dev/null
+
+if [ $? -eq 0 ]; then
+	chmod u+x target/release/rilo && echo "Release is successful!";
+else
+	return 1;
+fi
